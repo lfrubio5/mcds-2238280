3d5726658fa6e5d9c46b985d3bee8566511d59ba
rubioLeonardo <lfrubio5@misena.edu.co>
2021-02-08 21:03:28 -0500

    Creacion Semilla categories

---
+++ proyectoOfac/database/seeders/CategorySeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use \App\Models\Category;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Metodo Insert
+        DB::table('categories')->insert([
+			'name'  => 'Xbox One',
+			'description'     => 'lorem insup sit amet.',
+			
+        ]);
+
+        // metodo ORM
+        $cat = new Category;
+        $cat->name  = 'Xbox';
+        $cat->description = 'xbox lorem insup sit amet.';
+        $cat->save();
+
+        $cat = new Category;
+        $cat->name  = 'GamerPc';
+        $cat->description = 'Gamer pc lorem insup sit amet.';
+        $cat->save();
+    }
+}
