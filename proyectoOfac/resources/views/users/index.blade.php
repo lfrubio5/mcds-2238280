7e09c6f3918dc7bd5d1f82c32f21d318a0967248
lfrubio5 <lfrubio5@misena.edu.co>
2021-03-10 21:06:39 -0500

    Display none tablet and mobile

--- proyectoOfac/resources/views/users/index.blade.php
+++ proyectoOfac/resources/views/users/index.blade.php
@@ -1,49 +1,51 @@
-
 @extends('layouts.app')
 
 @section('title', 'Module Users')
 
 @section('content')
     <div class="container">
-        <div class="col-md-10 offset-md-1">
-            <h1><i class="fas fa-users">Mudile users</i></h1>
-            <hr>
-            <a hreft="" class="btn btn-success"><i class="fas -ga-plus">Add Users</i></a>
-            <hr>
-            <table class="table table-striped table-border table-hover">
-                <thead>
-                    <tr>
-                        <th>Nombre Completo</th>
-                        <th>Photo</th>
-                        <th>Email</th>
-                        <th>role</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($users as $key => $user)
-                    <tr>
-                        <td>{{ $user->fullname }}</td>
-                        <td><img src="{{ asset($user->photo) }}" width="36px" alt=""></td>
-                        <td>{{ $user->email }}</td>
-                        <td>{{ $user->role }}</td>
-                        <td class="text-center">
-
-                            <a href="" class="btn btn-sm btn-larapp">
-                                <i class="fas fa-search"></i>
-                            </a>
-                            <a href="" class="btn btn-sm btn-larapp">
-                                <i class="fas fa-pen"></i>
-                            </a>
-                            <a href="" class="btn btn-sm btn-danger">
-                                <i class="fas fa-trash"></i>
-                            </a>
-                        </td>
-                    </tr> 
-                    @endforeach
-                </tbody>
-            </table>
-            {{ $users->links() }}
+        <div class="row">
+            <div class="col-md-10 offset-md-1">
+                <h1> <i class="fas fa-users"></i> Module Users</h1>
+                <hr>
+                <a href="" class="btn btn-success"> 
+                    <i class="fas fa-plus"></i> Add Users 
+                </a>
+                <br><br>
+                <table class="table table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th>Fullname</th>
+                            <th class="d-none d-md-table-cell">Email</th>
+                            <th>Photo</th>
+                            <th class="d-none d-md-table-cell">Role</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($users as $user)
+                            <tr>
+                                <td>{{ $user->fullname }}</td>
+                                <td class="d-none d-md-table-cell">{{ $user->email }}</td>
+                                <td><img src="{{ asset($user->photo) }}" width="36px"></td>
+                                <td class="d-none d-md-table-cell">{{ $user->role }}</td>
+                                <td>
+                                    <a href="" class="btn btn-sm btn-larapp">
+                                        <i class="fas fa-search"></i>
+                                    </a>
+                                    <a href="" class="btn btn-sm btn-larapp">
+                                        <i class="fas fa-pen"></i>
+                                    </a>
+                                    <a href="" class="btn btn-sm btn-danger">
+                                        <i class="fas fa-trash"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                {{ $users->links() }}
+            </div>
         </div>
     </div>
 @endsection
