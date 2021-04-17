44a93c0675c347b543bd564853a45ce98fbca1b0
lfrubio5 <lfrubio5@misena.edu.co>
2021-04-16 21:11:29 -0500

    show game solution conflict

---
+++ proyectoOfac/app/Http/Controllers/GameController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Game;
+use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\User;
+use App\Http\Requests\GameRequest;
+use Auth;
+
+class GameController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $games = Game::paginate(20);
+        return view('games.index')->with('games', $games);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories = Category::all();
+        return view('games.create')->with('categories', $categories);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(GameRequest $request)
+    {
+        $game = new Game;
+        $game->name = $request->name;
+        if ($request->hasFile('image')) {
+            $file = time() . '.' . $request->image->extension();
+            $request->image->move(public_path('imgs'), $file);
+            $game->image = 'imgs/' . $file;
+        }
+        $game->description = $request->description;
+        $game->user_id     = Auth::user()->id;
+        $game->category_id = $request->category_id;
+        $game->slider      = $request->slider;
+        $game->price       = $request->price;
+
+        if ($game->save()) {
+            return redirect('games')->with('message', 'The Game: ' . $game->name . ' was successfully added');
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Game  $game
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Game $game)
+    {
+        $user = User::find($game->user_id);
+        $categories = Category::find($game->category_id);
+        return view('games.show')->with('categories', $categories)
+                                 ->with('game', $game)->with('user',$user);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Game  $game
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Game $game)
+    {
+        $categories = Category::all();
+        return view('games.edit')->with('categories', $categories)
+                                 ->with('game', $game);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Game  $game
+     * @return \Illuminate\Http\Response
+     */
+    public function update(GameRequest $request, Game $game)
+    {
+        $game->name  = $request->name;
+        if ($request->hasFile('image')) {
+            $file = time() . '.' . $request->image->extension();
+            $request->image->move(public_path('imgs'), $file);
+            $game->image = 'imgs/' . $file;
+        }
+        $game->description = $request->description;
+        $game->category_id = $request->category_id;
+        $game->slider      = $request->slider;
+        $game->price       = $request->price;
+
+        if ($game->save()) {
+            return redirect('games')->with('message', 'The Game: ' . $game->name . ' was successfully edited');
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Game  $game
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Game $game)
+    {
+        if ($game->delete()) {
+            return redirect('games')->with('message', 'The Game: ' . $game->name . ' was successfully deleted');
+        }
+    }
+
+    public function search(Request $request)
+    {
+        $games = Game::names($request->q)->orderBy('id', 'DESC')->paginate(20);
+        return view('games.search')->with('games', $games);
+    }
+}
